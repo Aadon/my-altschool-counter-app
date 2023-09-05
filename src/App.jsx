ac5bd25455f5e93e69764f25b503ac99f11f2f91
Aadon <ngwujnr@gmail.com>
2023-09-06 00:52:23 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,52 @@
+import { Fragment, useState } from 'react'
+import './index.css'
+import { ChevronUp } from "react-feather";
+import { ChevronsUp } from "react-feather";
+import { RotateCcw } from "react-feather";
+import { Hash } from 'react-feather'
+import { ChevronsDown } from "react-feather";
+import { ChevronDown } from "react-feather";
+
+function App() {
+  const [count, setCount] = useState(0)
+  
+  const styleCount = () => {
+    return count < 0 ? "red" : count > 0 ? "green" : "blue";
+  };
+  return (
+    <section className="container">
+      <p>Current Value</p>
+      <h1 
+      style={{ color: styleCount() }}
+      >
+        {count}
+        </h1>
+      <div className="button-wrapper">
+        <button onClick={() => setCount((count) => count + 1)}>
+          <ChevronUp />
+        </button>
+        <button onClick={() => setCount((count) => count + 10)}>
+          <ChevronsUp />
+        </button>
+        <button onClick={() => setCount((count) => (count = 0))}>
+          <RotateCcw />
+        </button>
+        <button
+          onClick={() =>
+            setCount((count) => (count = Math.floor(Math.random() * 100))) + 1
+          }
+        >
+          <Hash />
+        </button>
+        <button onClick={() => setCount((count) => count - 10)}>
+          <ChevronsDown />
+        </button>
+        <button onClick={() => setCount((count) => count - 1)}>
+          <ChevronDown />
+        </button>
+      </div>
+    </section>
+  );
+}
+
+export default App
